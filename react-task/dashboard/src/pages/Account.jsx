3f1e82c95a760f8d208e952e3dcf6bf2a1bb8544
virendra kumar <virendrajangir563@gmail.com>
2026-09-25 09:13:27 +0530

    dashboard first commit

---
+++ react-task/dashboard/src/pages/Account.jsx
@@ -0,0 +1,10 @@
+function Account() {
+  return (
+    <div>
+      <h1>Account</h1>
+      <p>Account Page</p>
+    </div>
+  );
+}
+
+export default Account;
---
+++ react-task/dashboard/src/pages/Integrations.jsx
@@ -0,0 +1,10 @@
+function Integrations() {
+  return (
+    <div>
+      <h1>Integrations</h1>
+      <p>Integrations Page</p>
+    </div>
+  );
+}
+
+export default Integrations;
